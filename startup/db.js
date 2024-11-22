4445fd3f0ce1dac069b0d92567b15fa69fd437b5
zagozagoo <vivizago4884@gmail.com>
2024-11-22 09:28:14 -0300

    VAI

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config')
 
 module.exports = function () {
-    const db = config.get('db');
+    // const db = config.get('db');
+    const db = process.env.DB_URL
     mongoose.connect(db)
         .then(() => console.log(`connected to ${db}`));
 }
